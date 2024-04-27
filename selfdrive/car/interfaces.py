f81486df25ee37603eb340b29bc9074d4edf7cb0
SangJo Kim <kandrea@nate.com>
2024-04-27 09:36:46 +0900

    apply replace()

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -155,6 +155,10 @@ def get_nn_model_path(car, eps_firmware) -> Tuple[Union[str, None, float]]:
           max_similarity = similarity_score
           model_path = os.path.join(TORQUE_NN_MODEL_PATH, f)
     return model_path, max_similarity
+
+  car1 = car.replace('_', ' ')
+  car1 = car1.replace(' HEV', ' HYBRID')
+  car = car1.replace('EV ', 'ELECTRIC ')
   print("########get_nn_model_path :", car, eps_firmware)
   if len(eps_firmware) > 3:
     eps_firmware = eps_firmware.replace("\\", "")
