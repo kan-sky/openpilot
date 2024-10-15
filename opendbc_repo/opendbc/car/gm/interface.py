14ac0dee43ce6a2ef1d098416fd97ae3b12593be
SangJo Kim <kandrea@nate.com>
2024-10-15 09:04:35 +0900

    toggle use Torque(Volt)

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -167,12 +167,15 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelay = 0.5  # large delay to initially start braking
 
     if candidate == CAR.CHEVROLET_VOLT:
-      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-      #ret.lateralTuning.pid.kpBP = [0., 40.]
-      #ret.lateralTuning.pid.kpV = [0., 0.17]
-      #ret.lateralTuning.pid.kiBP = [0.]
-      #ret.lateralTuning.pid.kiV = [0.]
-      #ret.lateralTuning.pid.kf = 1.  # get_steer_feedforward_volt()
+      useTorque = Params().get_bool("LateralTorqueCustom")
+      if useTorque:
+        CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      else:
+        ret.lateralTuning.pid.kpBP = [0., 40.]
+        ret.lateralTuning.pid.kpV = [0., 0.17]
+        ret.lateralTuning.pid.kiBP = [0.]
+        ret.lateralTuning.pid.kiV = [0.]
+        ret.lateralTuning.pid.kf = 1.  # get_steer_feedforward_volt()
       ret.steerActuatorDelay = 0.2
 
     elif candidate == CAR.GMC_ACADIA:
