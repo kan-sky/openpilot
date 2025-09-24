4f0c666b5aa05dfcaf4789e35225890fb448b8c5
SangJo Kim <kandrea@nate.com>
2025-09-24 20:41:12 +0900

    fix

--- selfdrive/carrot/carrot_serv.py
+++ selfdrive/carrot/carrot_serv.py
@@ -814,7 +814,7 @@ class CarrotServ:
       speed_n_sources.append((route_speed, "route"))
       #speed_n_sources.append((self.calculate_current_speed(dist, speed * self.mapTurnSpeedFactor, 0, 1.2), "route"))
 
-    model_turn_speed = sm['modelV2'].meta.modelTurnSpeed
+    model_turn_speed = max(sm['modelV2'].meta.modelTurnSpeed, self.autoCurveSpeedLowerLimit)
     if model_turn_speed < 200 and abs(vturn_speed) < 120:
       speed_n_sources.append((model_turn_speed, "model"))
 
