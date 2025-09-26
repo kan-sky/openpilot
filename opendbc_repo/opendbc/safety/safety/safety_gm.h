535a158c989937ccd4722f36a2205bdba8452cae
SangJo Kim <kandrea@nate.com>
2025-09-26 10:41:37 +0900

    fix

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -224,7 +224,7 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
     int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
     bool allowed_btn = (button == GM_BTN_CANCEL) && cruise_engaged_prev;
 
-    if ((gm_hw == GM_ASCM) || gm_force_ascm || gm_cam_long || gm_pedal_long) {
+    if ((gm_hw == GM_ASCM) || (gm_hw == GM_SDCM) || gm_cam_long || gm_pedal_long) {
       allowed_btn |= (button == GM_BTN_SET || button == GM_BTN_RESUME || button == GM_BTN_UNPRESS);
     }
     if (gm_cc_long || gm_pcm_cruise) {
