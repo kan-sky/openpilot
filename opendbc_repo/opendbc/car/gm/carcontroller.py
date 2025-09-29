11cea2393b241a403c0404af0bafe6e9e2962be9
SangJo Kim <kandrea@nate.com>
2025-09-29 14:25:33 +0900

    fix

--- opendbc_repo/opendbc/car/gm/carcontroller.py
+++ opendbc_repo/opendbc/car/gm/carcontroller.py
@@ -159,7 +159,7 @@ class CarController(CarControllerBase):
     if self.CP.openpilotLongitudinalControl:
       signal_stop_detected = CS.out.vEgo < 0.1 and CS.lead_distance == float('inf')  # 신호정지중
       lead_departed = CS.lead_distance < 45.0 and CS.lead_speed > 0.5  # 리드카 출발
-      rolling_back = CS.out.vEgo > 0.5 and CS.out.aEgo < -0.3  # 정지후 뒤로 밀리고 있는 상태
+      rolling_back = CS.out.aEgo < -0.05  # 정지후 뒤로 밀리고 있는 상태
       # Gas/regen, brakes, and UI commands - all at 25Hz
       if self.frame % 4 == 0:
         friction_sent_this_tick = False
