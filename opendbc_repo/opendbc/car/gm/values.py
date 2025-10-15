6e399f1141018b74eabdecd2bcbc47049fd4f057
eunhopapa1 <baboom4@naver.com>
2025-10-15 21:35:33 +0900

    Update values.py

--- opendbc_repo/opendbc/car/gm/values.py
+++ opendbc_repo/opendbc/car/gm/values.py
@@ -245,7 +245,7 @@ class CAR(Platforms):
   )
   CHEVROLET_TRAX = GMPlatformConfig(
     [GMCarDocs("Chevrolet TRAX 2024")],
-    CarSpecs(mass=1365, wheelbase=2.7, steerRatio=16.1, centerToFrontRatio=0.7),
+    CarSpecs(mass=1365, wheelbase=2.7, steerRatio=16.1, centerToFrontRatio=0.6),
   )
   CADILLAC_CT6_ACC = GMPlatformConfig(
     [GMCarDocs("CT6-2019 Advanced ACC", "Adaptive Cruise Control (ACC)")],
