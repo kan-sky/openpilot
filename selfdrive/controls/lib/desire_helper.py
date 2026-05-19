1538f19ee9cbb048c15d52a3a236ebe7546225ad
kans <kandrea@nate.com>
2026-05-19 21:34:07 +0900

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -392,15 +392,15 @@ class DesireHelper:
         self.lane_change_state = LaneChangeState.laneChangeFinishing
 
 
-        elif self.lane_change_state == LaneChangeState.laneChangeFinishing:
-          self.lane_change_ll_prob = min(self.lane_change_ll_prob + DT_MDL, 1.0)
-          if self.lane_change_ll_prob > 0.99:
-            self.lane_change_direction = LaneChangeDirection.none
-            if desire_enabled:
-              self.lane_change_state = LaneChangeState.preLaneChange
-              self.next_lane_change = True
-            else:
-              self.lane_change_state = LaneChangeState.off
+      elif self.lane_change_state == LaneChangeState.laneChangeFinishing:
+        self.lane_change_ll_prob = min(self.lane_change_ll_prob + DT_MDL, 1.0)
+        if self.lane_change_ll_prob > 0.99:
+          self.lane_change_direction = LaneChangeDirection.none
+          if desire_enabled:
+            self.lane_change_state = LaneChangeState.preLaneChange
+            self.next_lane_change = True
+          else:
+            self.lane_change_state = LaneChangeState.off
 
     # timer
     if self.lane_change_state in (LaneChangeState.off, LaneChangeState.preLaneChange):
