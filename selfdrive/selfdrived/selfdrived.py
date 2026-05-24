7033515d4f58a48c49473c58878aa388a297358e
kans <kandrea@nate.com>
2026-05-24 14:51:01 +0900

    Update selfdrived.py

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -383,17 +383,22 @@ class SelfdriveD:
     elif not CS.canValid:
       self.events.add(EventName.canError)
 
+    # Kans: forbid commIssue while AutoResume is trying
     # generic catch-all. ideally, a more specific event should be added above instead
+    auto_resume_trying = self.params_memory.get_bool("AutoResumeTrying")
+    ignore_comm_issue_for_resume = auto_resume_trying and (CS.standstill or CS.vEgo < 0.5)
+
     has_disable_events = self.events.contains(ET.NO_ENTRY) and (self.events.contains(ET.SOFT_DISABLE) or self.events.contains(ET.IMMEDIATE_DISABLE))
     no_system_errors = (not has_disable_events) or (len(self.events) == num_events)
-    if not self.sm.all_checks() and no_system_errors:
-      if not self.sm.all_alive():
-        self.events.add(EventName.commIssue)
-      elif not self.sm.all_freq_ok():
-        self.events.add(EventName.commIssueAvgFreq)
-      else:
-        self.events.add(EventName.commIssue)
 
+    if not self.sm.all_checks() and no_system_errors:
+      if not ignore_comm_issue_for_resume:
+        if not self.sm.all_alive():
+          self.events.add(EventName.commIssue)
+        elif not self.sm.all_freq_ok():
+          self.events.add(EventName.commIssueAvgFreq)
+        else:
+          self.events.add(EventName.commIssue)
       logs = {
         'invalid': [s for s, valid in self.sm.valid.items() if not valid],
         'not_alive': [s for s, alive in self.sm.alive.items() if not alive],
