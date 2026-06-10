1a1e3a9739f4ce295020abb860a9e9479eade2aa
kans <kandrea@nate.com>
2026-06-10 23:53:30 +0900

    Update carrot_serv.py

--- selfdrive/carrot/carrot_serv.py
+++ selfdrive/carrot/carrot_serv.py
@@ -767,6 +767,7 @@ class CarrotServ:
       start_fork_dist = 5.0
       start_turn_dist = 5.0
       atc_debug = "Utn"
+      
     elif is_rotary:
       start_fork_dist = 15.0
       start_turn_dist = 15.0
