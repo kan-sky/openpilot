e0205e300534700eac6750dd236ed8d46f2e2279
kans <kandrea@nate.com>
2026-08-10 13:55:08 +0900

    Update lat_mpc.py

--- openpilot/selfdrive/controls/lib/lateral_mpc_lib/lat_mpc.py
+++ openpilot/selfdrive/controls/lib/lateral_mpc_lib/lat_mpc.py
@@ -166,10 +166,6 @@ class LateralMpc:
       self.solver.cost_set(i, 'W', W)
     self.solver.cost_set(N, 'W', W[:COST_E_DIM,:COST_E_DIM])
 
-  # Kans: spped offset
-  def get_speed_offset(self, v_ego):
-    v = np.clip(v_ego, 10.0, 32.0)
-    return float(np.interp(v, [10.0, 20.0, 32.0], [8.0, 9.0, 10.0]))
   def run(self, x0, p, y_pts, heading_pts, yaw_rate_pts):
     x0_cp = np.copy(x0)
     p_cp = np.copy(p)
@@ -177,13 +173,9 @@ class LateralMpc:
     self.solver.constraints_set(0, "ubx", x0_cp)
     self.yref[:,0] = y_pts
     v_ego = p_cp[0, 0]
-    # Kans
-    speed_offset = self.get_speed_offset(v_ego)
-    v_ego_offset = v_ego + speed_offset
-
-    self.yref[:,1] = heading_pts * v_ego_offset
-    self.yref[:,2] = yaw_rate_pts * v_ego_offset
-
+    # rotation_radius = p_cp[1]
+    self.yref[:,1] = heading_pts * (v_ego + SPEED_OFFSET)
+    self.yref[:,2] = yaw_rate_pts * (v_ego + SPEED_OFFSET)
     for i in range(N):
       self.solver.cost_set(i, "yref", self.yref[i])
       self.solver.set(i, "p", p_cp[i])
