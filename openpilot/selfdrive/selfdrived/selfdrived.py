b426c745cee9a1c9868a6f7e8937f305d5ef1db0
kan-sky <kandrea@nate.com>
2026-08-18 15:38:48 +0900

    fix autoHold

--- openpilot/selfdrive/selfdrived/selfdrived.py
+++ openpilot/selfdrive/selfdrived/selfdrived.py
@@ -252,7 +252,7 @@ class SelfdriveD:
         (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
         self.events.add(EventName.pedalPressed)
 
-      if CS.autoHoldActivated:
+      if CS.autoHoldActive:
         self.events.add(EventName.autoHold)
 
     # Create events for temperature, disk space, and memory
